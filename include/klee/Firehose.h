eafa1ecc988f5490c546dd7e2acb835a0107ab6a
Marko Dimjašević <marko@cs.utah.edu>
2016-06-10 23:43:50 -0600

    A first implementation of (almost) all Firehose data structures

--- include/klee/Firehose.h
+++ include/klee/Firehose.h
@@ -23,25 +23,35 @@
 namespace klee {
   namespace firehose {
 
+    // Dummy string value used for default reference parameter values
+    static const std::string dummyString("");
+
     class XML {
+    private:
+      static bool isNonEmpty(const std::string& s) { return s != ""; }
+    protected:
+      const std::string mkString(std::vector<std::string> ss,
+				 std::string sep = "\n") const;
     public:
-      virtual std::string toXML() const = 0;
+      virtual const std::string toXML() const = 0;
+      XML() {}
     };
 
     class Point: public XML {
     private:
       unsigned m_column;
       unsigned m_line;
-    public: 
-      Point(); // This one is unfortunately needed in C++, though I
-      	       // don't want the constructor public.
+    public:
       Point(unsigned column, unsigned line);
       Point(const Point& that);
-      Point& operator=(const Point& that);
       unsigned getColumn() const;
       unsigned getLine() const;
-      std::string toXML() const;
+      const std::string toXML() const;
     };
+
+    // Dummy point value
+    static const Point dummyPoint = Point(0, 0);
+
     
     class Range: public XML {
     private:
@@ -49,58 +59,67 @@ namespace klee {
     public:
       Range(const Point& p1, const Point& p2);
       Range(const Range& that);
-      Range(); // This one is unfortunately needed in C++, though I
-      	       // don't want the constructor public.
-      Range& operator=(const Range& that);
-      Point getP1() const;
-      Point getP2() const;
-      std::string toXML() const;
+      const Point& getP1() const;
+      const Point& getP2() const;
+      const std::string toXML() const;
     };
+
+    // Dummy range value
+    static const Range dummyRange = Range(dummyPoint, dummyPoint);
+
     
     class File: public XML {
     private:
       std::string m_path;
     public:
       File(const std::string& path);
-      File(); // This one is unfortunately needed in C++, though I
-	      // don't want the constructor public.
       File(const char* path);
       File(const File& that);
-      File& operator=(const File& that);
-      std::string getPath() const;
-      std::string toXML() const;
+      const std::string& getPath() const;
+      const std::string toXML() const;
     };
+
+    // Dummy file value
+    static const File dummyFile(dummyString);
+
     
     class Function: public XML {
     private:
       std::string m_name;
     public: 
       Function(const std::string& name);
-      Function(); // This one is unfortunately needed in C++, though I
-		  // don't want the constructor public.
       Function(const char* name);
       Function(const Function& that);
-      Function& operator=(const Function& that);
-      std::string getName() const;
-      std::string toXML() const;
+      const std::string& getName() const;
+      const std::string toXML() const;
     };
+
+    // Dummy function value
+    static const Function dummyFunction(dummyString);
+    
 
     class Location: public XML {
     private:
       File m_file;
       Function m_function;
       Range m_range;
+      Point m_point;
     public:
-      Location(const File& file, const Function& function, const Range& range);
+      Location(const File& file, const Function& function = dummyFunction,
+	       const Range& range = dummyRange);
+      Location(const File& file, const Function& function = dummyFunction,
+	       const Point& point = dummyPoint);
       Location(const Location& that);
-      Location(); // This one is unfortunately needed in C++, though I
-		  // don't want the constructor public.
-      Location& operator=(const Location& that);
-      File getFile() const;
-      Function getFunction() const;
-      Range getRange() const;
-      std::string toXML() const;
+      const File& getFile() const;
+      const Function& getFunction() const;
+      const Range& getRange() const;
+      const Point& getPoint() const;
+      const std::string toXML() const;
     };
+
+    //Dummy location value
+    static const Location dummyLocation(dummyFile, dummyFunction, dummyRange);
+
     
     class State: public XML {
     private:
@@ -108,12 +127,13 @@ namespace klee {
     public:
       State(const Location& location);
       State(const State& that);
-      State(); // This one is unfortunately needed in C++, though I
-	       // don't want the constructor public.
-      State& operator=(const State& that);
-      Location getLocation() const;
-      std::string toXML() const;
+      const Location& getLocation() const;
+      const std::string toXML() const;
     };
+
+    // Dummy location value
+    static const State dummyState(dummyLocation);
+    
 
     class Trace: public XML {
     private:
@@ -121,36 +141,113 @@ namespace klee {
     public:
       Trace(const std::vector<State>& states);
       Trace(const Trace& that);
-      Trace(); // This one is unfortunately needed in C++, though I
-	       // don't want the constructor public.
-      Trace& operator=(const Trace& that);
-      std::vector<State> getStates() const;
-      std::string toXML() const;
+      const std::vector<State>& getStates() const;
+      const std::string toXML() const;
     };
 
-    // class Analysis {
-    // };
+    // Dummy trace value
+    static const Trace dummyTrace(std::vector<State>(1, dummyState));
+
     
-    // class Metadata {
-    // };
+    class Message: public XML {
+    private:
+      std::string m_msg;
+    public:
+      Message(const std::string& msg);
+      Message(const Message& that);
+      const std::string& get() const;
+      const std::string toXML() const;
+    };
+
+    // Dummy message value
+    static const Message dummyMessage(dummyString);
+
     
-    // class Generator {
-    // };
+    class Issue: public XML {
+    private:
+      Message m_message;
+      Location m_location;
+      Trace m_trace;
+    public:
+      Issue(const Message& message, const Location& location,
+	    const Trace& trace = dummyTrace);
+      Issue(const Issue& that);
+      const Message& getMessage() const;
+      const Location& getLocation() const;
+      const Trace& getTrace() const;
+      const std::string toXML() const;
+    };
+
+    // Dummy issue value
+    static const Issue dummyIssue(dummyMessage, dummyLocation, dummyTrace);
     
-    // class SUT {
-    // };
+
+    class Results: public XML {
+    private:
+      std::vector<Issue> m_issues;
+    public:
+      Results(const std::vector<Issue>& issues);
+      Results(const Results& that);
+      const std::vector<Issue>& getIssues() const;
+      const std::string toXML() const;
+    };
+
+    static const Results dummyResults(std::vector<Issue>(1, dummyIssue));
     
-    // class DebianSource {
-    // };
+
+    class Generator: public XML {
+    private:
+      std::string m_name;
+      std::string m_version;
+    public:
+      Generator(const std::string& name, const std::string& version);
+      Generator(const Generator& that);
+      const std::string& getName() const;
+      const std::string& getVersion() const;
+      const std::string toXML() const;
+    };
+
+    // Dummy generator value
+    static const Generator dummyGenerator(dummyString, dummyString);
+
+
+    class SUT: public XML {
+      // TODO: implement this class and add it to as a member of the
+      // Metadata class
+    };
+
+
+    class Metadata: public XML {
+    private:
+      Generator m_generator;
+      // SUT m_sut;
+    public:
+      Metadata(const Generator& generator);
+      // Metadata(const Generator& generator, const SUT& sut);
+      Metadata(const Metadata& that);
+      const Generator& getGenerator() const;
+      // const SUT& getSUT() const;
+      const std::string toXML() const;
+    };
+
+    // Dummy metadata value
+    static const Metadata dummyMetadata(dummyGenerator);
+
     
-    // class Results {
-    // };
-    
-    // class Issue {
-    // };
-    
-    // class Message {
-    // };    
+    class Analysis: public XML {
+    private:
+      Metadata m_metadata;
+      Results m_results;
+    public:
+      Analysis(const Metadata& metadata, const Results& results);
+      Analysis(const Analysis& that);
+      const Metadata& getMetadata() const;
+      const Results& getResults() const;
+      const std::string toXML() const;
+    };
+
+    // Dummy analysis value
+    static const Analysis dummyAnalysis(dummyMetadata, dummyResults);
   }
 }
 
--- lib/Support/Firehose.cpp
+++ lib/Support/Firehose.cpp
@@ -9,15 +9,30 @@
 
 #include "klee/Firehose.h"
 
+#include <algorithm>
 #include <iostream>
 #include <sstream>
 
 using namespace klee::firehose;
 
 
-Point::Point(unsigned column, unsigned line): m_column(column), m_line(line) {}
+const std::string XML::mkString(std::vector<std::string> ss,
+				std::string sep) const {
+  std::vector<std::string> filtered(ss.size());
+  std::copy_if(ss.begin(), ss.end(), filtered.begin(), isNonEmpty);
+  
+  std::ostringstream os;
+  for(unsigned i = 0; i < filtered.size() - 1; ++i)
+    os << filtered[i] << sep;
+  if (filtered.size() > 0)
+    os << filtered[filtered.size() - 1];
+  
+  return os.str();
+}
 
-Point::Point(): m_column(0), m_line(0) {}
+
+Point::Point(unsigned column, unsigned line):
+  m_column(column), m_line(line) {}
 
 unsigned Point::getColumn() const { return this->m_column; }
 unsigned Point::getLine() const   { return this->m_line; }
@@ -27,150 +42,254 @@ Point::Point(const Point& that) {
   this->m_line = that.getLine();
 }
 
-Point& Point::operator=(const Point& that) {
-  this->m_column = that.getColumn();
-  this->m_line = that.getLine();
-  return *this;
+const std::string Point::toXML() const {
+  if (this != &dummyPoint)
+    return std::string("<point column=\"" + std::to_string(this->getColumn()) +
+		       "\" line=\"" + std::to_string(this->getLine())
+		       + "\"/>");
+  else return "";
 }
 
-std::string Point::toXML() const {
-  return std::string("<point column=\"" + std::to_string(this->getColumn()) +
-		     "\" line=\"" + std::to_string(this->getLine()) + "\"/>");
-}
+Range::Range(const Point& p1, const Point& p2): m_p1(p1), m_p2(p2) {}
 
-Range::Range(const Point& p1, const Point& p2) {
-  this->m_p1 = p1;
-  this->m_p2 = p2;
-}
+Range::Range(const Range& that): m_p1(that.getP1()), m_p2(that.getP2()) {}
 
-Range::Range() {}
+const Point& Range::getP1() const { return this->m_p1; }
+const Point& Range::getP2() const { return this->m_p2; }
 
-Range::Range(const Range& that) {
-  this->m_p1 = that.getP1();
-  this->m_p2 = that.getP2();
-}
-
-Range& Range::operator=(const Range& that) {
-  this->m_p1 = that.getP1();
-  this->m_p2 = that.getP2();
-  return *this;
-}
-
-Point Range::getP1() const { return this->m_p1; }
-Point Range::getP2() const { return this->m_p2; }
-
-std::string Range::toXML() const {
-  return std::string("<range>\n" +
-		     this->getP1().toXML() + "\n" +
-		     this->getP2().toXML() + "\n" +
-		     "</range>");
+const std::string Range::toXML() const {
+  if (this != &dummyRange) {
+    std::vector<std::string> r;
+    r.push_back("<range>");
+    r.push_back(this->getP1().toXML());
+    r.push_back(this->getP2().toXML());
+    r.push_back("</range>");
+    return mkString(r);
+  }
+  else return "";
 }
 
 
 File::File(const std::string& path): m_path(path) {}
 
-File::File(): m_path("") {}
-
 File::File(const char* path): m_path(path) {}
 
-std::string File::getPath() const { return this->m_path; }
+const std::string& File::getPath() const { return this->m_path; }
 
 File::File(const File& that) { this->m_path = that.getPath(); }
 
-File& File::operator=(const File& that) {
-  this->m_path = that.getPath();
-  return *this;
+const std::string File::toXML() const {
+  if (this != &dummyFile)
+    return std::string("<file given-path=\"" + this->getPath() + "\"/>");
+  else return "";
 }
 
-std::string File::toXML() const {
-  return std::string("<file given-path=\"" + this->getPath() + "\"/>");
-}
 
 Function::Function(const std::string& name): m_name(name) {}
 
-Function::Function(): m_name("") {}
-
 Function::Function(const char* name): m_name(name) {}
 
-std::string Function::getName() const { return this->m_name; }
+const std::string& Function::getName() const { return this->m_name; }
   
-Function::Function(const Function& that) { this->m_name = that.getName(); }
+Function::Function(const Function& that): m_name(that.getName()) {}
 
-Function& Function::operator=(const Function& that) {
-  this->m_name = that.getName();
-  return *this;
-}
-
-std::string Function::toXML() const {
-  return std::string("<function name=\"" + this->getName() + "\"/>");
+const std::string Function::toXML() const {
+  if (this != &dummyFunction)
+    return std::string("<function name=\"" + this->getName() + "\"/>");
+  else return "";
 }
 
 
 Location::Location(const File& file, const Function& function,
-		   const Range& range) {
-  this->m_file = file;
-  this->m_function = function;
-  this->m_range = range;
-}
+		   const Range& range):
+  m_file(file), m_function(function), m_range(range), m_point(dummyPoint) {}
 
-File Location::getFile() const { return this->m_file; }
-Function Location::getFunction() const { return this->m_function; }
-Range Location::getRange() const { return this->m_range; }
+Location::Location(const File& file, const Function& function,
+		   const Point& point):
+  m_file(file), m_function(function), m_range(dummyRange), m_point(point) {}
 
-Location::Location(const Location& that) {
-  this->m_file = that.getFile();
-  this->m_function = that.getFunction();
-  this->m_range = that.getRange();
-}
+const File& Location::getFile() const         { return this->m_file; }
+const Function& Location::getFunction() const { return this->m_function; }
+const Range& Location::getRange() const       { return this->m_range; }
+const Point& Location::getPoint() const       { return this->m_point; }
 
-Location::Location() {}
+Location::Location(const Location& that):
+  m_file(that.getFile()), m_function(that.getFunction()),
+  m_range(that.getRange()), m_point(that.getPoint()) {}
 
-Location& Location::operator=(const Location& that) {
-  this->m_file = that.getFile();
-  this->m_function = that.getFunction();
-  this->m_range = that.getRange();
-  return *this;
-}
-
-std::string Location::toXML() const {
-  return std::string("<location>\n" +
-		     this->getFile().toXML() + "\n" +
-		     this->getFunction().toXML() + "\n" +
-		     this->getRange().toXML() + "\n" +
-		     "</location>");
+const std::string Location::toXML() const {
+  if (this != &dummyLocation) {
+    std::vector<std::string> r;
+    r.push_back("<location>");
+    r.push_back(this->getFile().toXML());
+    r.push_back(this->getFunction().toXML());
+    r.push_back(this->getRange().toXML());
+    r.push_back(this->getPoint().toXML());
+    r.push_back("</location>");
+    return mkString(r);
+  }
+  else return "";
 }
 
 
-Trace::Trace(const std::vector<State>& states) {
-  this->m_states = std::vector<State>(states);
+State::State(const Location& location): m_location(location) {}
+
+const Location& State::getLocation() const { return this->m_location; }
+
+State::State(const State& that): m_location(that.getLocation()) {}
+
+const std::string State::toXML() const {
+  if (this != &dummyState) {
+    std::vector<std::string> r;
+    r.push_back("<state>");
+    r.push_back(this->getLocation().toXML());
+    r.push_back("</state>");
+    return mkString(r);
+  }
+  else return "";
 }
 
-Trace::Trace(const Trace& that) {
-  this->m_states = std::vector<State>(that.getStates());
+
+Trace::Trace(const std::vector<State>& states):
+  m_states(std::vector<State>(states)) {}
+
+Trace::Trace(const Trace& that):
+  m_states(std::vector<State>(that.getStates())) {}
+
+const std::vector<State>& Trace::getStates() const { return this->m_states; }
+
+const std::string Trace::toXML() const {
+  if (this != &dummyTrace) {
+    std::vector<State> tmp(this->getStates().begin(), this->getStates().end());
+    std::vector<std::string> r;
+    r.push_back("<trace>");
+    for(std::vector<State>::iterator iter = tmp.begin();
+	iter != tmp.end();
+	++iter)
+      r.push_back(iter->toXML());
+    r.push_back("</trace>");
+    return mkString(r);
+  }
+  else return "";
 }
 
-Trace::Trace() {}
+Message::Message(const std::string& msg): m_msg(msg) {}
 
-Trace& Trace::operator=(const Trace& that) {
-  this->m_states = std::vector<State>(that.getStates());
-  return *this;
+Message::Message(const Message& that): m_msg(that.get()) {}
+
+const std::string& Message::get() const { return this->m_msg; }
+
+const std::string Message::toXML() const {
+  if (this != &dummyMessage)
+    return std::string("<message>" + this->get() + "</message>");
+  else return "";
 }
 
-std::vector<State> Trace::getStates() const {
-  return this->m_states;
+
+Issue::Issue(const Message& message, const Location& location,
+	     const Trace& trace):
+  m_message(message), m_location(location), m_trace(trace) {}
+
+Issue::Issue(const Issue& that):
+  m_message(that.getMessage()), m_location(that.getLocation()),
+  m_trace(that.getTrace()) {}
+
+const Message& Issue::getMessage() const   { return this->m_message; }
+const Location& Issue::getLocation() const { return this->m_location; }
+const Trace& Issue::getTrace() const       { return this->m_trace; }
+
+const std::string Issue::toXML() const {
+  if (this != &dummyIssue) {
+    std::vector<std::string> r;
+    r.push_back("<issue>");
+    r.push_back(this->getMessage().toXML());
+    r.push_back(this->getLocation().toXML());
+    r.push_back(this->getTrace().toXML());
+    r.push_back("</issue>");
+    return mkString(r);
+  }
+  else return "";
 }
 
-std::string Trace::toXML() const {
-  std::ostringstream os;
 
-  os << "<trace>\n";
+Results::Results(const std::vector<Issue>& issues): m_issues(issues) {}
 
-  for(std::vector<State>::iterator iter = this->getStates().begin();
-      iter != this->getStates().end();
-      ++iter)
-    os << iter->toXML() + "\n";
+Results::Results(const Results& that): m_issues(that.getIssues()) {}
 
-  os << "</trace>";
+const std::vector<Issue>& Results::getIssues() const { return this->m_issues; }
 
-  return os.str();
+const std::string Results::toXML() const {
+  if (this != &dummyResults) {
+    std::vector<Issue> tmp(this->getIssues().begin(), this->getIssues().end());
+    std::vector<std::string> r;
+    r.push_back("<results>");
+    for(std::vector<Issue>::iterator iter = tmp.begin();
+	iter != tmp.end();
+	++iter)
+      r.push_back(iter->toXML());
+    r.push_back("</results>");
+    return mkString(r);
+  }
+  else return "";
+}
+
+
+Generator::Generator(const std::string& name, const std::string& version):
+  m_name(name), m_version(version) {}
+
+Generator::Generator(const Generator& that):
+  m_name(that.getName()), m_version(that.getVersion()) {}
+
+const std::string& Generator::getName() const    { return this->m_name; }
+const std::string& Generator::getVersion() const { return this->m_version; }
+
+const std::string Generator::toXML() const {
+  if (this != &dummyGenerator)
+    return std::string("<generator name=\"" + this->getName() +
+		       "\" version=\"" + this->getVersion() + "\"/>");
+  else return "";
+}
+
+
+Metadata::Metadata(const Generator& generator): m_generator(generator) {}
+// : m_sut(sut)
+
+Metadata::Metadata(const Metadata& that): m_generator(that.getGenerator()) {}
+// : m_sut(that.getSUT())
+
+const Generator& Metadata::getGenerator() const { return this->m_generator; }
+// const SUT& Metadata::getSUT() const             { return this->m_sut; }
+
+const std::string Metadata::toXML() const {
+  if (this != &dummyMetadata) {
+    std::vector<std::string> r;
+    r.push_back("<metadata>");
+    r.push_back(this->getGenerator().toXML());
+    // r.push_back(this->getSUT().toXML());
+    r.push_back("</metadata>");
+    return mkString(r);
+  }
+  else return "";
+}
+
+
+Analysis::Analysis(const Metadata& metadata, const Results& results):
+  m_metadata(metadata), m_results(results) {}
+
+Analysis::Analysis(const Analysis& that):
+  m_metadata(that.getMetadata()), m_results(that.getResults()) {}
+
+const Metadata& Analysis::getMetadata() const { return this->m_metadata; }
+const Results& Analysis::getResults() const   { return this->m_results; }
+
+const std::string Analysis::toXML() const {
+  if (this != &dummyAnalysis) {
+    std::vector<std::string> r;
+    r.push_back("<analysis>");
+    r.push_back(this->getMetadata().toXML());
+    r.push_back(this->getResults().toXML());
+    r.push_back("</analysis>");
+    return mkString(r);
+  }
+  else return "";
 }
